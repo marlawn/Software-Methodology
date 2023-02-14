c60b5960f331df05e717cad82fd181a5e86b4fd3
Marlon Vergara <93688231+marlawn@users.noreply.github.com>
2023-02-13 20:24:49 -0500

    Delete Project 1 - OOP with Java directory

--- Project 1 - OOP with Java/Project 1 JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"oop","l":"Constants"},{"p":"oop","l":"Date"},{"p":"oop","l":"Major"},{"p":"oop","l":"Profile"},{"p":"oop","l":"Roster"},{"p":"oop","l":"RosterManager"},{"p":"oop","l":"RunProject1"},{"p":"oop","l":"Student"}];updateSearchResults();
